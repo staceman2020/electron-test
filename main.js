06c2c29a1ae331bdc2b59cae69d328e7882c6d08
staceman2020 <48968803+staceman2020@users.noreply.github.com>
2024-02-20 15:11:00 +1300

    Staged development

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const { app, BrowserWindow } = require("electron");
 const { ipcMain } = require("electron");
 const path = require("path");
 const fs = require("fs");
-let win;
+// var winax = require("winax");
+let winClaims;
 function createWindow() {
-  win = new BrowserWindow({
+  winClaims = new BrowserWindow({
     title: "Claims Management",
     width: 800,
     height: 600,
+
     webPreferences: {
       nodeIntegration: true,
       preload: path.join(__dirname, "preload.js"),
@@ -15,26 +17,26 @@ function createWindow() {
   });
 
   //win.loadFile("index.html");
-  win.loadURL("http://localhost:5173/claims/list").then(
+  winClaims.loadURL("http://localhost:5180/claims/list").then(
     () => {
-      win.webContents.openDevTools();
-      win.webContents.on("did-start-navigation", (event, url) => {
-        console.log("Navigated to: " + url);
-        event.preventDefault();
-      });
-      win.webContents.on("did-finish-load", () => {});
-      win.webContents.on("found-in-page", (result) => {
-        console.log("Found in page: " + JSON.stringify(result, null, 2));
-      });
+      winClaims.webContents.openDevTools();
+      // win.webContents.on("did-start-navigation", (event, url) => {
+      //   console.log("Navigated to: " + url);
+      //   event.preventDefault();
+      // });
+      // win.webContents.on("did-finish-load", () => {});
+      // win.webContents.on("found-in-page", (result) => {
+      //   console.log("Found in page: " + JSON.stringify(result, null, 2));
+      // });
 
-      win.webContents.executeJavaScript("alert('told you so');");
-      setInterval(() => {
-        console.log("Sending ping");
-        win.webContents.findInPage("claim");
-        win.webContents.emit("message", "whoooooooh!");
+      // win.webContents.executeJavaScript("alert('told you so');");
+      // setInterval(() => {
+      //   console.log("Sending ping");
+      //   win.webContents.findInPage("claim");
+      //   win.webContents.emit("message", "whoooooooh!");
 
-        // send("message", "whoooooooh!");
-      }, 2000);
+      //   // send("message", "whoooooooh!");
+      // }, 2000);
     },
     () => {
       console.log("Window is ready");
@@ -46,6 +48,9 @@ app.whenReady().then(createWindow);
 
 ipcMain.on("toMain", (event, args) => {
   console.log("Got it " + JSON.stringify(args, null, 2));
+  // var con = new winax.Object("Outlook.Application");
+  // var mail = con.CreateItem(0);
+
   fs.readFile("C:/data/scm/electron-omni/README.md", (error, data) => {
     // Do something with file contents
     let responseObj = {
@@ -55,7 +60,9 @@ ipcMain.on("toMain", (event, args) => {
 
     // Send result back to renderer process
     setInterval(() => {
-      win.webContents.send("fromMain", responseObj);
+      winClaims.webContents.send("fromMain", responseObj);
     }, 2000);
   });
 });
+
+ipcMain;
